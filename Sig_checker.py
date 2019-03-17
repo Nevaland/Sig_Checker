dcf381e7996a886db6235c3310e3972469bcda88
Nevaland <deanh0716@gmail.com>
2019-03-17 19:28:11 +0900

    First Commit

---
+++ Sig_checker.py
@@ -0,0 +1,12 @@
+# Signature Checker
+# Developer. Nevaland
+# Data. 19.03.17 - 19.03.17
+# Version. 1
+
+import os
+
+def usage():
+    print "Usage: "
+
+if __name__ == "__main__":
+    print "TEST"
